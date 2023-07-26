31d62db187f179841f920fbb73fe2e31a0600ffb
demosofa <thientndgcd18746@fpt.edu.vn>
2023-07-26 11:33:53 +0700

    fix: config

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,17 +1,12 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { Outlet } from 'react-router';
 import Navbar from './Navbar';
 import Footer from './Footer';
-
-const queryClient = new QueryClient();
 
 export default function Layout() {
   return (
     <>
       <Navbar />
-      <QueryClientProvider client={queryClient}>
-        <Outlet />
-      </QueryClientProvider>
+      <Outlet />
       <Footer />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 
 import routes from './routes';
 
+const queryClient = new QueryClient();
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
