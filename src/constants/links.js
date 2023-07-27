1b6922a27f39b17698b438d67bf88459d1b3f6b0
demosofa <62540039+demosofa@users.noreply.github.com>
2023-07-27 13:17:21 +0700

    style: footer and navbar (#5)

---
+++ src/constants/links.js
@@ -0,0 +1,11 @@
+export default [
+  { label: 'home', to: '/' },
+  { label: 'event', to: 'event' },
+  { label: 'workspace', to: 'workspace' },
+  { label: 'vouchers', to: 'vouchers' },
+  { label: 'faqs', to: 'faqs' },
+  { label: 'hot needs', to: 'hot-needs' },
+  { label: 'term and policies', to: 'term-policies' },
+  { label: 'about us', to: 'about-us' },
+  { label: 'contact us', to: 'contact-us' },
+];
--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -1,6 +1,7 @@
-import { Row, Col, Image } from 'antd';
+import { Row, Col, Image, Grid } from 'antd';
 import { Link } from 'react-router-dom';
 import './style.css';
+import links from 'constants/links';
 
 const logos = [
   {
@@ -29,18 +30,16 @@ const logos = [
       'https://conext-production.s3.ap-southeast-1.amazonaws.com/64271580_60163244_17378348_73394412.png',
   },
 ];
-
+const { useBreakpoint } = Grid;
 export default function Footer() {
+  const { lg } = useBreakpoint();
   return (
     <section className="footer">
       <Row
         gutter={[16, 16]}
         align="middle"
-        style={{
-          maxWidth: '83.125rem',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-        }}
+        justify="center"
+        style={{ padding: '1.5625rem 0' }}
       >
         {logos.map(({ to, image }) => (
           <Col span={8} lg={4} key={to} style={{ textAlign: 'center' }}>
@@ -54,7 +53,65 @@ export default function Footer() {
           </Col>
         ))}
       </Row>
-      <Row></Row>
+      <Row
+        justify="space-between"
+        wrap={false}
+        style={{ borderTop: '1px solid white', padding: '2.875rem 0' }}
+      >
+        <Col
+          span={12}
+          lg={4}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            marginRight: '5vw',
+          }}
+        >
+          <Link to="/">
+            <Image
+              style={{ maxHeight: '30px' }}
+              preview={false}
+              src="https://conext-production.s3.ap-southeast-1.amazonaws.com/logo_footer.png"
+            />
+          </Link>
+          <span
+            style={{
+              display: 'block',
+              fontSize: '.8125rem',
+              color: '#5c6369',
+              marginTop: '1.0625rem',
+            }}
+          >
+            © 2020 CoNEXT
+          </span>
+        </Col>
+        {lg ? (
+          <Col span={16}>
+            <Row wrap={true} align={'middle'} justify={'center'}>
+              {links.slice(1).map(({ label, to }) => (
+                <Col
+                  span={6}
+                  key={label}
+                  style={{
+                    padding: '0.9375rem !important',
+                    lineHeight: '.9375rem',
+                  }}
+                >
+                  <Link style={{ color: 'white' }} to={to}>
+                    {label.toUpperCase()}
+                  </Link>
+                </Col>
+              ))}
+            </Row>
+          </Col>
+        ) : null}
+        <Col span={12} lg={4} style={{ textAlign: 'center' }}>
+          <Link to="https://www.facebook.com/conext.asia">Facebook </Link>
+          <Link to="https://www.linkedin.com/company/conextvietnam">
+            LinkedIn
+          </Link>
+        </Col>
+      </Row>
     </section>
   );
 }
